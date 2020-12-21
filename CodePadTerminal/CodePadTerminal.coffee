246b24795e26185668e2ec85d441b27190229b29
lxsang <xsang.le@gmail.com>
2020-12-21 14:11:28 +0100

    Initial commit

---
+++ CodePadTerminal/CodePadTerminal.coffee
@@ -0,0 +1,129 @@
+# import the CodePad application module
+App = this.OS.application.CodePad
+
+html = """
+<div data-id="codepad-terminal-container"></div>
+"""
+
+# define the extension
+class App.extensions.CodePadTerminal extends App.BaseExtension
+    constructor: (app) ->
+        super app
+    
+    dependencies: () ->
+        [
+            "pkg://xTerm/main.js",
+            "pkg://xTerm/main.css",
+            "pkg://Antunnel/main.js"
+        ]
+
+    open: () ->
+        return @notify __("Antunnel service is not available") unless window.Antunnel and Antunnel.tunnel
+        return @notify __("xTerm library is not available") unless Terminal
+        
+        return @description.domel.selected = true if @description
+        # create html element
+        scheme = $.parseHTML(html)[0]
+        @description = {
+            text: __("Terminal"),
+            iconclass: "fa fa-terminal",
+            container: scheme
+        }
+        @app.showBottomBar(true)
+        @app.bottombar.addTab(@description, true)
+        @term = new Terminal { cursorBlink: true }
+        @fitAddon = new FitAddon.FitAddon()
+        @term.loadAddon(@fitAddon)
+        @term.setOption('fontSize', '12')
+        @term.open scheme
+        @sub = undefined
+        @term.onKey (d) =>
+            return unless @sub
+            @sub.send Antunnel.Msg.DATA, new TextEncoder("utf-8").encode(d.key)
+        
+        scheme.contextmenuHandle = (e, m) =>
+            m.items = [
+                { text: "__(Copy)", id: "copy" },
+                { text: "__(Paste)", id: "paste"}
+            ]
+            m.onmenuselect = (e) =>
+                return unless e
+                @mctxHandle e.data.item.data
+            m.show e
+            
+        @resizeContent()
+        @resizefn = (e) =>
+            @resizeContent()
+        @app.on "resize", @resizefn
+        @openSession()
+    
+    resizeContent: (e) ->
+        @fitAddon.fit()
+        ncol = @term.cols
+        nrow = @term.rows
+        return unless @sub
+        arr = new Uint8Array(8)
+        arr.set Antunnel.Msg.bytes_of(ncol), 0
+        arr.set Antunnel.Msg.bytes_of(nrow), 4
+        @sub.send Antunnel.Msg.CTRL, arr
+    
+    mctxHandle: (data) ->
+        switch data.id
+            when "paste"
+                cb = (text) =>
+                    return unless text and text isnt ""
+                    text = text
+                            .replace /\r/g, ""
+                            .replace /\n/g, "\r"
+                    @sub.send Antunnel.Msg.DATA, new TextEncoder("utf-8").encode(text) if @sub
+                    @term.focus()
+                    
+                @app._api.getClipboard()
+                    .then (text) =>
+                        cb(text)
+                    .catch (e) =>
+                        @error __("Unable to paste"), e
+                        #ask for user to enter the text manually
+                        @app.openDialog("TextDialog", { title: "Paste text"})
+                            .then (text) =>
+                                cb(text)
+                            .catch (err) => @error err.toString(), err
+            when "copy"
+                text = @term.getSelection()
+                return unless text and text isnt ""
+                @app._api.setClipboard text
+            else
+
+    openSession: () ->
+        @term.clear()
+        @term.focus()
+        @sub = new Antunnel.Subscriber("vterm")
+        @sub.onopen = () =>
+            console.log("Subscribed")
+            @resizeContent (($ @mterm).width()) ,  (($ @mterm).height())
+            @term.focus()
+        
+        @sub.onerror = (e) =>
+            @error __("Unable to connect to: vterm"), e
+            @sub = undefined
+            @cleanup()
+
+        @sub.onmessage =  (e) =>
+            @term.write(new TextDecoder("utf-8").decode(e.data)) if @term and e.data
+        
+        @sub.onclose = () =>
+            @sub = undefined
+            @notify __("Terminal connection closed")
+            @cleanup()
+        
+        Antunnel.tunnel.subscribe @sub
+    
+    cleanup: () ->
+        if(@resizefn and @app.observable)
+            @app.off "resize", @resizefn
+        @app.bottombar.selectedIndex = 0
+        @sub.close() if @sub
+        @sub = undefined
+        return unless @description
+        @app.bottombar.removeTab(@description.domel)
+        @description = undefined
---
+++ CodePadTerminal/build/debug/CodePadTerminal.js
@@ -0,0 +1,175 @@
+(function() {
+  // import the CodePad application module
+  var App, html;
+
+  App = this.OS.application.CodePad;
+
+  html = `<div data-id="codepad-terminal-container"></div>`;
+
+  // define the extension
+  App.extensions.CodePadTerminal = class CodePadTerminal extends App.BaseExtension {
+    constructor(app) {
+      super(app);
+    }
+
+    dependencies() {
+      return ["pkg://xTerm/main.js", "pkg://xTerm/main.css", "pkg://Antunnel/main.js"];
+    }
+
+    open() {
+      var scheme;
+      if (!(window.Antunnel && Antunnel.tunnel)) {
+        return this.notify(__("Antunnel service is not available"));
+      }
+      if (!Terminal) {
+        return this.notify(__("xTerm library is not available"));
+      }
+      if (this.description) {
+        return this.description.domel.selected = true;
+      }
+      // create html element
+      scheme = $.parseHTML(html)[0];
+      this.description = {
+        text: __("Terminal"),
+        iconclass: "fa fa-terminal",
+        container: scheme
+      };
+      this.app.showBottomBar(true);
+      this.app.bottombar.addTab(this.description, true);
+      this.term = new Terminal({
+        cursorBlink: true
+      });
+      this.fitAddon = new FitAddon.FitAddon();
+      this.term.loadAddon(this.fitAddon);
+      this.term.setOption('fontSize', '12');
+      this.term.open(scheme);
+      this.sub = void 0;
+      this.term.onKey((d) => {
+        if (!this.sub) {
+          return;
+        }
+        return this.sub.send(Antunnel.Msg.DATA, new TextEncoder("utf-8").encode(d.key));
+      });
+      scheme.contextmenuHandle = (e, m) => {
+        m.items = [
+          {
+            text: "__(Copy)",
+            id: "copy"
+          },
+          {
+            text: "__(Paste)",
+            id: "paste"
+          }
+        ];
+        m.onmenuselect = (e) => {
+          if (!e) {
+            return;
+          }
+          return this.mctxHandle(e.data.item.data);
+        };
+        return m.show(e);
+      };
+      this.resizeContent();
+      this.resizefn = (e) => {
+        return this.resizeContent();
+      };
+      this.app.on("resize", this.resizefn);
+      return this.openSession();
+    }
+
+    resizeContent(e) {
+      var arr, ncol, nrow;
+      this.fitAddon.fit();
+      ncol = this.term.cols;
+      nrow = this.term.rows;
+      if (!this.sub) {
+        return;
+      }
+      arr = new Uint8Array(8);
+      arr.set(Antunnel.Msg.bytes_of(ncol), 0);
+      arr.set(Antunnel.Msg.bytes_of(nrow), 4);
+      return this.sub.send(Antunnel.Msg.CTRL, arr);
+    }
+
+    mctxHandle(data) {
+      var cb, text;
+      switch (data.id) {
+        case "paste":
+          cb = (text) => {
+            if (!(text && text !== "")) {
+              return;
+            }
+            text = text.replace(/\r/g, "").replace(/\n/g, "\r");
+            if (this.sub) {
+              this.sub.send(Antunnel.Msg.DATA, new TextEncoder("utf-8").encode(text));
+            }
+            return this.term.focus();
+          };
+          return this.app._api.getClipboard().then((text) => {
+            return cb(text);
+          }).catch((e) => {
+            this.error(__("Unable to paste"), e);
+            //ask for user to enter the text manually
+            return this.app.openDialog("TextDialog", {
+              title: "Paste text"
+            }).then((text) => {
+              return cb(text);
+            }).catch((err) => {
+              return this.error(err.toString(), err);
+            });
+          });
+        case "copy":
+          text = this.term.getSelection();
+          if (!(text && text !== "")) {
+            return;
+          }
+          return this.app._api.setClipboard(text);
+      }
+    }
+
+    openSession() {
+      this.term.clear();
+      this.term.focus();
+      this.sub = new Antunnel.Subscriber("vterm");
+      this.sub.onopen = () => {
+        console.log("Subscribed");
+        this.resizeContent(($(this.mterm)).width(), ($(this.mterm)).height());
+        return this.term.focus();
+      };
+      this.sub.onerror = (e) => {
+        this.error(__("Unable to connect to: vterm"), e);
+        this.sub = void 0;
+        return this.cleanup();
+      };
+      this.sub.onmessage = (e) => {
+        if (this.term && e.data) {
+          return this.term.write(new TextDecoder("utf-8").decode(e.data));
+        }
+      };
+      this.sub.onclose = () => {
+        this.sub = void 0;
+        this.notify(__("Terminal connection closed"));
+        return this.cleanup();
+      };
+      return Antunnel.tunnel.subscribe(this.sub);
+    }
+
+    cleanup() {
+      if (this.resizefn && this.app.observable) {
+        this.app.off("resize", this.resizefn);
+      }
+      this.app.bottombar.selectedIndex = 0;
+      if (this.sub) {
+        this.sub.close();
+      }
+      this.sub = void 0;
+      if (!this.description) {
+        return;
+      }
+      this.app.bottombar.removeTab(this.description.domel);
+      return this.description = void 0;
+    }
+
+  };
+
+}).call(this);
